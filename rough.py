0702e35b439a39a5ced8290a14cce8adbfe91ac0
Nikhil Maddikunta <33166520+nmaddikunta21@users.noreply.github.com>
2023-04-18 19:41:27 -0400

    rough.py
    streamlit app

--- rough.py
+++ rough.py
@@ -1,14 +1,67 @@
-import schedule
-import subprocess
-import time
+import os
+import streamlit as st
+import pandas as pd
+import zipfile
 
-def run_script():
-    # Replace 'script.sh' with the name of your shell script
-    subprocess.call('./script.sh', shell=True)
+# Define a function to load the first DataFrame from a folder based on user input
+def load_dataframe1(inputs):
+    # Get a list of all CSV files in the data folder
+    files = [f for f in os.listdir('data') if f.endswith('.csv')]
+    
+    # Load each CSV file into a DataFrame and concatenate them
+    df = pd.concat([pd.read_csv(os.path.join('data', f)) for f in files])
+    
+    # Filter the DataFrame based on the user's input
+    if inputs:
+        df = df[df['column'].isin(inputs)]
+    
+    return df
 
-# Schedule the script to run at 7 AM every day
-schedule.every().day.at("07:00").do(run_script)
+# Define a function to load the second DataFrame from a folder based on user input
+def load_dataframe2(inputs):
+    # Get a list of all CSV files in the data2 folder
+    files = [f for f in os.listdir('data2') if f.endswith('.csv')]
+    
+    # Load each CSV file into a DataFrame and concatenate them
+    df = pd.concat([pd.read_csv(os.path.join('data2', f)) for f in files])
+    
+    # Filter the DataFrame based on the user's input
+    if inputs:
+        df = df[df['column'].isin(inputs)]
+    
+    return df
 
-while True:
-    schedule.run_pending()
-    time.sleep(1)
+# Set the app title
+st.title("My App")
+
+# Add a sidebar with an input field for the first DataFrame
+inputs1 = st.sidebar.text_input("Enter values separated by commas")
+st.sidebar.subheader("Input Field 1")
+
+# Add a sidebar with an input field for the second DataFrame
+inputs2 = st.sidebar.text_input("Enter values separated by commas")
+st.sidebar.subheader("Input Field 2")
+
+# Load the first DataFrame based on the user's input, if given
+if inputs1:
+    df1 = load_dataframe1([x.strip() for x in inputs1.split(',')])
+    st.write("First DataFrame")
+    st.write(df1)
+    
+    # Add a button to download the first DataFrame, but only if inputs were given
+    if st.button("Download First DataFrame"):
+        with zipfile.ZipFile("first_dataframe.zip", "w") as zip:
+            zip.writestr("first_dataframe.csv", df1.to_csv(index=False))
+        st.markdown("Download the first DataFrame: [first_dataframe.zip](first_dataframe.zip)")
+
+# Load the second DataFrame based on the user's input, if given
+if inputs2:
+    df2 = load_dataframe2([x.strip() for x in inputs2.split(',')])
+    st.write("Second DataFrame")
+    st.write(df2)
+    
+    # Add a button to download the second DataFrame, but only if inputs were given
+    if st.button("Download Second DataFrame"):
+        with zipfile.ZipFile("second_dataframe.zip", "w") as zip:
+            zip.writestr("second_dataframe.csv", df2.to_csv(index=False))
+        st.markdown("Download the second DataFrame: [second_dataframe.zip](second_dataframe.zip)")
